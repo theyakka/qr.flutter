5388d0ef44b36726d978a34341eecbc99819a7fc
Luke Freeman <freeman.luke@gmail.com>
2019-06-06 11:18:04 -0700

    export qrdart so you can access types easily

--- lib/qr_flutter.dart
+++ lib/qr_flutter.dart
@@ -3,5 +3,8 @@
  * Copyright (c) 2019 the QR.Flutter authors.
  * See LICENSE for distribution and usage details.
  */
+
+export 'package:qr/qr.dart';
+
 export 'src/qr_image.dart';
 export 'src/qr_painter.dart';
