da8c0c00642dd860dece5757925d211881f5d5a2
Luke Pighetti <lukepighetti@gmail.com>
2019-07-04 08:15:41 -0400

    dead code removal, refactor to provider

--- example/lib/screens/content_widget.dart
+++ example/lib/screens/content_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:flutter_qr_demo/screens/simple_options_screen.dart';
-import 'package:flutter_qr_demo/widgets/option_button.dart';
+import 'package:flutter_qr_demo/screens/main.screen.dart';
+import 'package:provider/provider.dart';
+// import 'package:flutter/services.dart';
+// import './simple_options_screen.dart';
+import '../widgets/option_button.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import '../widgets/too_long_widget.dart';
 
@@ -12,87 +14,110 @@ class ContentWidget extends StatefulWidget {
 }
 
 class _ContentWidgetState extends State<ContentWidget> {
-  static const double _entryRowHeight = 70.0;
-
-  final _optionsScreenKey = GlobalKey<SimpleOptionsScreenState>();
-
-  String _dataString = 'Hello from this QR code!';
-  String _inputErrorText;
-  final TextEditingController _textController = TextEditingController();
-  List<Widget> _stackChildren;
-
-  @override
-  void initState() {
-    _stackChildren = <Widget>[_qrContent()];
-    super.initState();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async {
-        if (_stackChildren.length > 1) {
-          _popOptionsScreen();
-          return false;
-        }
-        return true;
-      },
-      child: Stack(
-        children: _stackChildren,
+    return Container(
+      color: const Color(0xFFFFFFFF),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Container(
+            color: Color(0xff8d42f5),
+            child: Column(
+              children: <Widget>[
+                EntryRow(),
+                OptionsRow(),
+                Divider(
+                  color: const Color(0xff5927a1),
+                  height: 1,
+                ),
+              ],
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: QrImage(
+                  data: Provider.of<MainState>(context).text,
+                  gapless: Provider.of<MainState>(context).gapless,
+                  errorCorrectionLevel:
+                      Provider.of<MainState>(context).errorCorrectionLevel,
+                  foregroundColor: const Color(0xFF111111),
+                  constrainErrorBounds: false,
+                  errorStateBuilder: (ctx, ex) {
+                    return ContentTooLongWidget();
+                  },
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
+}
 
-  Widget _qrContent() {
-    return SafeArea(
-      top: true,
-      bottom: true,
-      child: Container(
-        color: const Color(0xFFFFFFFF),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+class OptionsRow extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Color(0xff8d42f5),
+      child: Padding(
+        padding: EdgeInsets.only(left: 30, right: 30, bottom: 10),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: <Widget>[
-            Container(
-              color: Color(0xff8d42f5),
-              child: Column(
-                children: <Widget>[
-                  _entryRow(),
-                  _optionsRow(),
-                  Divider(
-                    color: const Color(0xff5927a1),
-                    height: 1,
-                  ),
-                ],
-              ),
+            OptionButton(
+              imageAssetName: 'assets/images/ic_version_circle.png',
+              title: 'GAPLESS',
+              value: Provider.of<MainState>(context).gapless ? "Yes" : "No",
+              onTapped: () => Provider.of<MainState>(context).toggleGapless(),
             ),
-            Expanded(
-              child: Center(
-                child: Padding(
-                  padding: const EdgeInsets.all(20),
-                  child: QrImage(
-                    data: _dataString,
-                    gapless: false,
-                    errorCorrectionLevel: QrErrorCorrectLevel.L,
-                    foregroundColor: const Color(0xFF111111),
-                    constrainErrorBounds: false,
-                    errorStateBuilder: (ctx, ex) {
-                      return ContentTooLongWidget();
-                    },
-                  ),
-                ),
-              ),
+            OptionButton(
+              imageAssetName: 'assets/images/ic_error_correct.png',
+              title: 'ERROR CORRECTION',
+              value: Provider.of<MainState>(context).errorCorrectionString,
+              onTapped: () => Provider.of<MainState>(context)
+                  .increaseErrorCorrectionLevel(),
             ),
           ],
         ),
       ),
     );
   }
+}
 
-  Widget _entryRow() {
+class EntryRow extends StatefulWidget {
+  final double entryRowHeight;
+
+  EntryRow({this.entryRowHeight = 70.0});
+
+  @override
+  _EntryRowState createState() => _EntryRowState();
+}
+
+class _EntryRowState extends State<EntryRow> {
+  TextEditingController _textController;
+
+  @override
+  void initState() {
+    _textController = TextEditingController();
+
+    _textController.addListener(() {
+      Provider.of<MainState>(context).text = _textController.text;
+    });
+
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.only(left: 30, right: 20),
       child: Container(
-        height: _entryRowHeight,
+        height: widget.entryRowHeight,
         child: Row(
           mainAxisSize: MainAxisSize.max,
           crossAxisAlignment: CrossAxisAlignment.center,
@@ -116,7 +141,6 @@ class _ContentWidgetState extends State<ContentWidget> {
                   focusedBorder: UnderlineInputBorder(
                     borderSide: BorderSide(color: const Color(0xffd1b4fa)),
                   ),
-                  errorText: _inputErrorText,
                 ),
               ),
             ),
@@ -126,22 +150,22 @@ class _ContentWidgetState extends State<ContentWidget> {
                 height: 50,
                 child: FlatButton(
                   color: const Color(0x11FFFFFF),
+                  disabledTextColor: Colors.grey,
                   highlightColor: const Color(0x11FFFFFF),
                   splashColor: const Color(0x33FFFFFF),
                   child: const Text(
-                    'MAKE IT',
+                    'Clear',
                     style: TextStyle(
                       color: Color(0xFFeaff00),
                       fontWeight: FontWeight.w700,
                       fontSize: 16,
                     ),
                   ),
-                  onPressed: () {
-                    setState(() {
-                      _dataString = _textController.text;
-                      _inputErrorText = null;
-                    });
-                  },
+                  onPressed: Provider.of<MainState>(context).text.isEmpty
+                      ? null
+                      : () {
+                          _textController.clear();
+                        },
                 ),
               ),
             )
@@ -149,73 +173,5 @@ class _ContentWidgetState extends State<ContentWidget> {
         ),
       ),
     );
-  }
-
-  Widget _optionsRow() {
-    return Container(
-      color: Color(0xff8d42f5),
-      child: Padding(
-        padding: EdgeInsets.only(left: 30, right: 30, bottom: 10),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            OptionButton(
-              imageAssetName: 'assets/images/ic_version_circle.png',
-              title: 'VERSION',
-              value: 'Auto',
-              onTapped: _showOptions,
-            ),
-            OptionButton(
-              imageAssetName: 'assets/images/ic_error_correct.png',
-              title: 'ERROR CORRECTION',
-              value: 'Low',
-              onTapped: _showOptions,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  void _showErrorSnackbar(BuildContext ctx, Exception ex) {
-//    if (ex is InputTooLongException) {
-//      _inputErrorText = 'Error! Maybe your input value is too long?';
-    Scaffold.of(ctx).showSnackBar(
-      SnackBar(content: Text(_inputErrorText)),
-    );
-//    }
-  }
-
-  void _showOptions() {
-    FocusScope.of(context).requestFocus(FocusNode());
-    setState(() {
-      _stackChildren.add(SimpleOptionsScreen(key: _optionsScreenKey));
-      SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-        systemNavigationBarColor: const Color(0xff8d42f5),
-        systemNavigationBarIconBrightness: Brightness.light,
-      ));
-    });
-  }
-
-  void _qrErrHandler(Exception ex) {
-    print('[QR] ERROR - $ex');
-    setState(() {
-      _inputErrorText = "Oops! Content is too long.";
-    });
-    _showErrorSnackbar(context, ex);
-  }
-
-  /// Pop the options screen (animate out and remove from the stack).
-  void _popOptionsScreen() {
-    _optionsScreenKey.currentState.toggle(callback: () {
-      setState(() {
-        _stackChildren.removeLast();
-      });
-    });
-    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
-      systemNavigationBarColor: Colors.white,
-      systemNavigationBarIconBrightness: Brightness.dark,
-    ));
   }
 }
--- example/lib/screens/main.screen.dart
+++ example/lib/screens/main.screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:qr_flutter/qr_flutter.dart';
 import 'content_widget.dart';
 
 /// The main screen of the application. This is the screen you see when the
@@ -11,9 +13,64 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: ContentWidget(),
-      resizeToAvoidBottomPadding: true,
+    return ChangeNotifierProvider<MainState>(
+      builder: (_) => MainState(),
+      child: Scaffold(
+        body: SafeArea(
+          child: ContentWidget(),
+        ),
+        resizeToAvoidBottomPadding: true,
+      ),
     );
+  }
+}
+
+/// The partnering state class for MainScreen
+class MainState with ChangeNotifier {
+  /// QR text
+  String _text = "";
+
+  String get text => _text;
+
+  set text(String s) {
+    _text = s;
+    notifyListeners();
+  }
+
+  /// QR error correction level
+  int _errorCorrectionLevel = QrErrorCorrectLevel.L;
+
+  int get errorCorrectionLevel => _errorCorrectionLevel;
+
+  String get errorCorrectionString =>
+      QrErrorCorrectLevel.getName(_errorCorrectionLevel);
+
+  increaseErrorCorrectionLevel() {
+    switch (_errorCorrectionLevel) {
+      case QrErrorCorrectLevel.L:
+        _errorCorrectionLevel = QrErrorCorrectLevel.M;
+        break;
+      case QrErrorCorrectLevel.M:
+        _errorCorrectionLevel = QrErrorCorrectLevel.Q;
+        break;
+      case QrErrorCorrectLevel.Q:
+        _errorCorrectionLevel = QrErrorCorrectLevel.H;
+        break;
+      case QrErrorCorrectLevel.H:
+      default:
+        _errorCorrectionLevel = QrErrorCorrectLevel.L;
+    }
+
+    notifyListeners();
+  }
+
+  /// QR gapless
+  bool _gapless = true;
+
+  bool get gapless => _gapless;
+
+  void toggleGapless() {
+    _gapless = !_gapless;
+    notifyListeners();
   }
 }
