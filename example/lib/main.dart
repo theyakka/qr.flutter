848af5a62577a991a838650dd6d7ee1b66055103
Saleem <saleem@ghost.cgocable.net>
2019-08-22 17:12:30 -0400

    Readded example app

---
+++ example/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'main_screen.dart';
+
+void main() => runApp(ExampleApp());
+
+/// The example application class
+class ExampleApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      statusBarIconBrightness: Brightness.dark,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+    ));
+    return MaterialApp(
+      title: 'QR.Flutter',
+      theme: ThemeData.light(),
+      debugShowCheckedModeBanner: false,
+      home: MainScreen(),
+    );
+  }
+}
