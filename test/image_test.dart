c9a3b70c54fc79f5046176147e37c7325043d155
Ankur <ankur.jain@nsitonline.in>
2020-07-08 13:52:52 -0700

    Fix tests broken due to PR #93 while adding semantics label.
    Need to wrap all widgets in test to use MaterialApp to provide directionalty.

--- test/image_test.dart
+++ test/image_test.dart
@@ -13,91 +13,99 @@ import 'package:qr_flutter/qr_flutter.dart';
 
 void main() {
   testWidgets('QrImage generates correct image', (tester) async {
-    final qrImage = Center(
-      child: RepaintBoundary(
-        child: QrImage(
-          data: 'This is a test image',
-          version: QrVersions.auto,
-          gapless: true,
-          errorCorrectionLevel: QrErrorCorrectLevel.L,
-        ),
-      ),
-    );
-    await tester.pumpWidget(qrImage);
-    await expectLater(
-      find.byType(RepaintBoundary),
-      matchesGoldenFile('./.golden/qr_image_golden.png'),
-    );
-  });
-
-  testWidgets('QrImage generates correct image with eye style', (tester) async {
-    final qrImage = Center(
-      child: RepaintBoundary(
-        child: QrImage(
-          data: 'This is a test image',
-          version: QrVersions.auto,
-          gapless: true,
-          errorCorrectionLevel: QrErrorCorrectLevel.L,
-          eyeStyle: const QrEyeStyle(
-            eyeShape: QrEyeShape.circle,
-            color: Colors.green,
+    final qrImage = MaterialApp(
+      home: Center(
+        child: RepaintBoundary(
+          child: QrImage(
+            data: 'This is a test image',
+            version: QrVersions.auto,
+            gapless: true,
+            errorCorrectionLevel: QrErrorCorrectLevel.L,
           ),
         ),
       ),
     );
     await tester.pumpWidget(qrImage);
     await expectLater(
-      find.byType(RepaintBoundary),
+      find.byType(QrImage),
+      matchesGoldenFile('./.golden/qr_image_golden.png'),
+    );
+  });
+
+  testWidgets('QrImage generates correct image with eye style', (tester) async {
+    final qrImage = MaterialApp(
+      home: Center(
+        child: RepaintBoundary(
+          child: QrImage(
+            data: 'This is a test image',
+            version: QrVersions.auto,
+            gapless: true,
+            errorCorrectionLevel: QrErrorCorrectLevel.L,
+            eyeStyle: const QrEyeStyle(
+              eyeShape: QrEyeShape.circle,
+              color: Colors.green,
+            ),
+          ),
+        ),
+      ),
+    );
+    await tester.pumpWidget(qrImage);
+    await expectLater(
+      find.byType(QrImage),
       matchesGoldenFile('./.golden/qr_image_eye_styled_golden.png'),
     );
   });
 
   testWidgets('QrImage generates correct image with data module style',
       (tester) async {
-    final qrImage = Center(
-      child: RepaintBoundary(
-        child: QrImage(
-          data: 'This is a test image',
-          version: QrVersions.auto,
-          gapless: true,
-          errorCorrectionLevel: QrErrorCorrectLevel.L,
-          dataModuleStyle: const QrDataModuleStyle(
-            dataModuleShape: QrDataModuleShape.circle,
-            color: Colors.blue,
+    final qrImage = MaterialApp(
+      home: Center(
+        child: RepaintBoundary(
+          child: QrImage(
+            data: 'This is a test image',
+            version: QrVersions.auto,
+            gapless: true,
+            errorCorrectionLevel: QrErrorCorrectLevel.L,
+            dataModuleStyle: const QrDataModuleStyle(
+              dataModuleShape: QrDataModuleShape.circle,
+              color: Colors.blue,
+            ),
           ),
         ),
       ),
     );
     await tester.pumpWidget(qrImage);
     await expectLater(
-      find.byType(RepaintBoundary),
+      find.byType(QrImage),
       matchesGoldenFile('./.golden/qr_image_data_module_styled_golden.png'),
     );
   });
 
   testWidgets('QrImage generates correct image with eye and data module sytle',
       (tester) async {
-    final qrImage = Center(
-      child: RepaintBoundary(
-        child: QrImage(
-          data: 'This is a test image',
-          version: QrVersions.auto,
-          gapless: true,
-          errorCorrectionLevel: QrErrorCorrectLevel.L,
-          eyeStyle: const QrEyeStyle(
-            eyeShape: QrEyeShape.circle,
-            color: Colors.green,
-          ),
-          dataModuleStyle: const QrDataModuleStyle(
-            dataModuleShape: QrDataModuleShape.circle,
-            color: Colors.blue,
+    final qrImage = MaterialApp(
+      home: Center(
+        child: RepaintBoundary(
+          child: QrImage(
+            data: 'This is a test image',
+            version: QrVersions.auto,
+            gapless: true,
+            errorCorrectionLevel: QrErrorCorrectLevel.L,
+            eyeStyle: const QrEyeStyle(
+              eyeShape: QrEyeShape.circle,
+              color: Colors.green,
+            ),
+            dataModuleStyle: const QrDataModuleStyle(
+              dataModuleShape: QrDataModuleShape.circle,
+              color: Colors.blue,
+            ),
           ),
         ),
       ),
     );
     await tester.pumpWidget(qrImage);
     await expectLater(
-      find.byType(RepaintBoundary),
+      find.byType(QrImage),
       matchesGoldenFile('./.golden/qr_image_eye_data_module_styled_golden.png'),
     );
   });
@@ -105,43 +113,47 @@ void main() {
   testWidgets(
       'QrImage does not apply eye and data module color when foreground '
       'color is also specified', (tester) async {
-    final qrImage = Center(
-      child: RepaintBoundary(
-        child: QrImage(
-          data: 'This is a test image',
-          version: QrVersions.auto,
-          gapless: true,
-          foregroundColor: Colors.red,
-          errorCorrectionLevel: QrErrorCorrectLevel.L,
-          eyeStyle: const QrEyeStyle(
-            eyeShape: QrEyeShape.circle,
-            color: Colors.green,
-          ),
-          dataModuleStyle: const QrDataModuleStyle(
-            dataModuleShape: QrDataModuleShape.circle,
-            color: Colors.blue,
+    final qrImage = MaterialApp(
+      home: Center(
+        child: RepaintBoundary(
+          child: QrImage(
+            data: 'This is a test image',
+            version: QrVersions.auto,
+            gapless: true,
+            foregroundColor: Colors.red,
+            errorCorrectionLevel: QrErrorCorrectLevel.L,
+            eyeStyle: const QrEyeStyle(
+              eyeShape: QrEyeShape.circle,
+              color: Colors.green,
+            ),
+            dataModuleStyle: const QrDataModuleStyle(
+              dataModuleShape: QrDataModuleShape.circle,
+              color: Colors.blue,
+            ),
           ),
         ),
       ),
     );
     await tester.pumpWidget(qrImage);
     await expectLater(
-      find.byType(RepaintBoundary),
+      find.byType(QrImage),
       matchesGoldenFile('./.golden/qr_image_foreground_colored_golden.png'),
     );
   });
 
   testWidgets('QrImage generates correct image with logo', (tester) async {
     final key = GlobalKey();
-    final qrImage = Center(
-      key: key,
-      child: RepaintBoundary(
-        child: QrImage(
-          data: 'This is a a qr code with a logo',
-          version: QrVersions.auto,
-          gapless: true,
-          errorCorrectionLevel: QrErrorCorrectLevel.L,
-          embeddedImage: FileImage(File('test/.images/logo_yakka.png')),
+    final qrImage = MaterialApp(
+      home: Center(
+        key: key,
+        child: RepaintBoundary(
+          child: QrImage(
+            data: 'This is a a qr code with a logo',
+            version: QrVersions.auto,
+            gapless: true,
+            errorCorrectionLevel: QrErrorCorrectLevel.L,
+            embeddedImage: FileImage(File('test/.images/logo_yakka.png')),
+          ),
         ),
       ),
     );
