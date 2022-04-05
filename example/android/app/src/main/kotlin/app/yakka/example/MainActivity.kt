4d04b8ff9f4bc82caac343950ad1fbc1bbe2ff2d
Luke Freeman <freeman.luke@gmail.com>
2022-04-04 17:19:39 -0700

    wip - adding to example

--- example/android/app/src/main/kotlin/app/yakka/example/MainActivity.kt
+++ example/android/app/src/main/kotlin/app/yakka/example/MainActivity.kt
@@ -1,6 +1,23 @@
 package app.yakka.example
 
+import android.os.Environment
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val channelName = "app.yakka.example/dirChannel"
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, channelName).setMethodCallHandler {
+                call, result ->
+            if (call.method == "getDownloads") {
+                result.success(
+                    mapOf("absPath" to Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).absolutePath),
+                )
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
 }
--- example/lib/save_file.dart
+++ example/lib/save_file.dart
@@ -1,1 +1,20 @@
-void saveData(List<int> data, String filename) {}
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:flutter/services.dart';
+import 'package:path_provider/path_provider.dart';
+
+void saveData(List<int> data, String filename) async {
+  const platform = MethodChannel("app.yakka.example/dirChannel");
+  final result = await platform.invokeMapMethod("getDownloads");
+  if (result != null) {
+    String absPath = result["absPath"];
+    String path = "$absPath/$filename";
+    final file = File(path);
+    try {
+      await file.writeAsBytes(data);
+    } catch (e) {
+      print(e);
+    }
+  }
+}
